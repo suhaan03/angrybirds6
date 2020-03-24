5727f3fb44c8622b97321add91311bf984041596
suhaan03 <suhaanshah03@gmail.com>
2020-03-24 11:50:47 +0530

    changes made

--- Bird.js
+++ Bird.js
@@ -2,12 +2,23 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.trajectory=[]
+    this.image1 = loadImage("sprites/smoke.png");
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+if(this.body.position.x>200 && this.body.speed>10) 
+{
+  var position=[this.body.position.x,this.body.position.y]
+  this.trajectory.push(position)
+}
 
-    super.display();
+for(var i =0;i<this.trajectory.length;i++){
+
+  image(this.image1,this.trajectory[i][0],this.trajectory[i][1] )
+}
+super.display();
   }
 }
--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gamestate="1"
 
 
 function preload() {
@@ -69,12 +70,15 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gamestate!=="END"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate="END"
 }
 
 function keyPressed(){
